f85ba2241c98321870f66235f47d8422a00db611
Naeem <naeemark@gmail.com>
2021-11-01 00:44:56 +0800

    Initial Commit: + Adds project structure + Adds test dependencies
     #1

---
+++ src/main/java/org/naeemrk/fcs/Application.java
@@ -0,0 +1,10 @@
+package org.naeemrk.fcs;
+
+/**
+ * @author Naeem <naeemark@gmail.com>
+ */
+public class Application {
+    public static void main(String[] args) {
+        System.out.println("Hello World...");
+    }
+}
---
+++ src/test/java/org/naeemrk/fcs/ApplicationTest.java
@@ -0,0 +1,15 @@
+package org.naeemrk.fcs;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author Naeem <naeemark@gmail.com>
+ */
+class ApplicationTest {
+
+    @Test
+    void main() {
+        // stub invocation of main method
+        Application.main(new String[]{});
+    }
+}
