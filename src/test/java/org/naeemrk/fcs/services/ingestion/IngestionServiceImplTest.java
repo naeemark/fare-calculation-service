ce8cc905dfdf1090abdda5edfe2b09493d1a1833
Naeem <naeemark@gmail.com>
2021-11-01 01:58:51 +0800

    + Adds stub layer for ingestion

---
+++ src/test/java/org/naeemrk/fcs/services/ingestion/IngestionServiceImplTest.java
@@ -0,0 +1,20 @@
+package org.naeemrk.fcs.services.ingestion;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * @author Naeem <naeemark@gmail.com>
+ */
+class IngestionServiceImplTest {
+
+    private final IngestionService service = new IngestionServiceImpl();
+
+    @Test
+    @DisplayName("Read data from provided file name")
+    void read() {
+        service.read("test file name");
+    }
+}
