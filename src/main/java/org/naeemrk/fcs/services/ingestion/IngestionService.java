056c4eae27a8adae7eba4198f71a1e347c003cf4
Naeem <naeemark@gmail.com>
2021-11-02 00:19:00 +0800

    + Reads file from resources

--- src/main/java/org/naeemrk/fcs/services/ingestion/IngestionService.java
+++ src/main/java/org/naeemrk/fcs/services/ingestion/IngestionService.java
@@ -1,10 +1,13 @@
 package org.naeemrk.fcs.services.ingestion;
 
 import org.naeemrk.fcs.common.Service;
+import org.naeemrk.fcs.models.Location;
+
+import java.util.List;
 
 /**
  * @author Naeem <naeemark@gmail.com>
  */
 public interface IngestionService extends Service {
-    void read(String fileName);
+    List<Location> read(String filePath);
 }
