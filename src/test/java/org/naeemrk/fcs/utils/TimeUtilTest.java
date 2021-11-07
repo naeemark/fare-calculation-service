e2c9d7d60aa227bef66945db2c6121026f2a7e3e
Naeem <naeemark@gmail.com>
2021-11-08 02:06:58 +0800

    Fix epoch testing values

--- src/test/java/org/naeemrk/fcs/utils/TimeUtilTest.java
+++ src/test/java/org/naeemrk/fcs/utils/TimeUtilTest.java
@@ -1,7 +1,10 @@
 package org.naeemrk.fcs.utils;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import java.time.*;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
@@ -11,24 +14,38 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
  */
 class TimeUtilTest {
 
+    private long midNight ;
+    private long peakTimeEpoch;
+    private long offPeakTimeEpoch;
+
+    @BeforeEach
+    void setUp() {
+        ZoneId zone = ZoneOffset.systemDefault();
+        ZonedDateTime zdt = ZonedDateTime.now(zone);
+        zdt = zdt.toLocalDate().atStartOfDay(zone);
+        midNight = zdt.toInstant().toEpochMilli() / 1000;
+        peakTimeEpoch = midNight + 10000;
+        offPeakTimeEpoch = peakTimeEpoch + 20000;
+    }
+
     @Test
     @DisplayName("isPeakTime: Yes")
     void isPeekTime_true() {
-        boolean peekTime = TimeUtil.isPeekTime(1636226859);
+        boolean peekTime = TimeUtil.isPeekTime(peakTimeEpoch);
         assertTrue(peekTime);
     }
 
     @Test
     @DisplayName("isPeakTime: No")
     void isPeekTime_false() {
-        boolean peekTime = TimeUtil.isPeekTime(1636286859);
+        boolean peekTime = TimeUtil.isPeekTime(offPeakTimeEpoch);
         assertFalse(peekTime);
     }
 
     @Test
     @DisplayName("isPeakTime: No")
     void isPeekTime_false_b() {
-        boolean peekTime = TimeUtil.isPeekTime(1636214400);
+        boolean peekTime = TimeUtil.isPeekTime(midNight);
         assertFalse(peekTime);
     }
 }
